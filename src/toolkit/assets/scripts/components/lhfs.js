cd2ae6bd6aa18182b990eb3d497fc31b69003b1d
Rocco R <xdemocle@gmail.com>
2015-02-06 18:17:50 +0100

    add scripts components folder

---
+++ src/toolkit/assets/scripts/components/lhfs.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  $( document ).ready(function() {
+    $(".tip").removeClass("visible");
+    $(".tip").addClass("visible");
+    $("li.payment").removeClass("data");
+    $("li.payment").addClass("data");
+  });
+});

b87674d8b06e08acba6b55f910d444519cb14e2a
Rocco R <xdemocle@gmail.com>
2015-02-23 15:21:28 +0100

    moving scripts folders under aegon-scripts-library as separate git submodule

--- src/assets/scripts/widgets/lhfs.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  $( document ).ready(function() {
-    $(".tip").removeClass("visible");
-    $(".tip").addClass("visible");
-    $("li.payment").removeClass("data");
-    $("li.payment").addClass("data");
-  });
-});

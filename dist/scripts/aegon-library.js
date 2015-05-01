d860a3034f16a76e38dd5b4af566804b66565514
yogibimbi <yogibimbi@qan.be>
2015-05-01 17:17:27 +0200

    include some generated dist files, plus updating the checkbox elements on lhfs and the example page, abstracted validation on checkboxes a bit further. TODO: make the required-asterisk also a general style element

--- dist/scripts/aegon-library.js
+++ dist/scripts/aegon-library.js
@@ -16472,6 +16472,26 @@ $.fn.msDropdown = $.fn.msDropDown; //make a copy
 /**
  * Example JavaScript component
  */
+// Closure with jQuery support
+(function($) {
+  'use strict';
+
+  // Add new item to public Drupal object
+  Drupal.behaviors["checkbox"] = {
+    attach: function () {
+    	var visited = function () {  //this is to implement a different error behaviour between when the form is loaded and once an element has been visited
+console.log("checkbox init");
+        $(this).addClass("visited");
+      };
+      $("span.checkbox").focus(visited);
+      $("span.checkbox").click(visited);
+    }
+  };
+})(jQuery);
+
+/**
+ * Example JavaScript component
+ */
 
 // Closure with jQuery support
 // (function($) {

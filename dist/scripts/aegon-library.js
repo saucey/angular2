3d69d2ab1440702803828c2717d8811d53c0a296
yogibimbi <yogibimbi@qan.be>
2015-05-05 18:07:50 +0200

    latest changes in the submodules propagated to the libraries, updated submodule heads

--- dist/scripts/aegon-library.js
+++ dist/scripts/aegon-library.js
@@ -17289,6 +17289,7 @@ console.log("text-input init");
 
 })(jQuery);
 
+/*jshint multistr: true */
 /**
  * User details script
  */
@@ -17302,78 +17303,79 @@ console.log("text-input init");
 
     attach: function (context, settings) {
 
+      // Cache the div
+      this.widget = $('#user_detail_widget');
+
       // Setup
       this.setup(settings);
-      
-      if (this.widget.length > 0) {
-        this.events();
-      }
     },
 
     setup: function (settings) {
 
-      this.widget = $('.user_detail_widget');
+      // Set url API
+      // this.apiUrl = settings.basePath + settings.pathToTheme +
+      //   '/includes/fake-widgets.php?id=';
+      // this.apiUrl = '/data/widgets/user_detail.json';
+      this.apiUrl = 'http://localhost:3000/data/widgets/user_detail.json';
+      // this.apiUrl = 'http://echo.jsontest.com/key/value/one/two';
 
-      // Setp url API
-      this.apiUrl = settings.basePath + settings.pathToTheme +
-        '/includes/fake-widgets.php?id=';
+      // If widget exist go ahead
+      if (this.widget.length > 0) {
+        this.events();
+      }
+
+      this.getUserData();
     },
 
-    showLogged: function () {
+    getDataType: function () {
 
-      // $("#shw-user-details").load(this.apiUrl + 'user');
-      // $('#shw-user-details').html('<div id="user_widget" class="user_widget">
-      //     <div id="user_widget_topmenu" class="topmenu">
-      //       <div class="username">Michiel van der Most Spijk</div>
-      //     </div>
+      var apiUrlOrig = this.apiUrl.split('/').splice(0, 3).join('/');
 
-      //     <div id="user_widget_dropdown" class="frame bubbleTopRight">
-      //       <div class="username">user name</div>
-      //       <div class="email">usermail@somesite.com</div>
-      //       <div class="lastlogin">Uw vorige bezoek was op 08-01-2015 om 14:11 uur</div>
-      //       <button class="arrow">Mijn overzicht</button>
-      //       <button class="white">uitloggen</button>
-      //     </div>
-      //   </div>');
+      return doc.location.origin.indexOf(apiUrlOrig) !== -1 ? 'json' : 'jsonp';
     },
 
-    simulation: function () {
+    getUserData: function () {
 
       var that = this;
 
-      that.showLogged();
+      $.ajax({
+        url: this.apiUrl,
+        dataType: this.getDataType(),
+        success: function(data){
+          var text = '';
+          var len = data.length;
 
-      // if (location.pathname.indexOf('mijnaegon') === -1) {
-      //   return;
-      // }
+          // Parse data into widget
+          that.parseWidget(data);
+        }
+      });
+    },
 
-      // setTimeout(function () {
+    parseWidget: function (data) {
 
-      //   if (window.confirm('Do you want simulate session logged?')) { 
+      $('span[data-id="user_detail_widget_name"]').text(data.username);
+      $('span[data-id="user_detail_widget_last_access"]').text(data.last_access);
 
-      //     setTimeout(function () {
-      //       that.showLogged();
-      //     }, 1000);
-      //   }
-
-      // }, 2000);
+      // console.log(data);
     },
 
     events: function () {
 
-      this.widget.find('button.btn-login').on('hover', function() {
+      // Hover on button login set class .off on itself and unbind
+      this.widget.find('button.btn-login-loggedin').on('hover', function() {
         $(this).addClass( "off" ).off('hover');
       });
 
-      this.widget.find('button.btn-login').on('click', function() {
+      // Click on button login toggle class .tap on itself
+      this.widget.find('button.btn-login-loggedin').on('click', function() {
         $(this).toggleClass("tap");
       });
 
-      this.widget.find('.highlight').one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
-
-        // console.log($(this));
-        $(this).parent().addClass('processed');
-
+      // In the end of animation of .highlight div, add class .processed to 
+      // widget's container to hide itself
+      this.widget.find('.highlight').one('webkitAnimationEnd oanimationend \
+        msAnimationEnd animationend', function() {
+          $(this).parents('.user_detail_widget').addClass('processed');
       });
     }
   };

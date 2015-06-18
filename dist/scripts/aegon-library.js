e293f51d49abb8f0af0ee9596457cc85440c81ff
yogibimbi <yogibimbi@qan.be>
2015-06-18 18:00:11 +0200

    updated dist and submodule HEADs, added content DIV for .feedback and discarded the firsst attempt to create a response for .feedback

--- dist/scripts/aegon-library.js
+++ dist/scripts/aegon-library.js
@@ -18463,6 +18463,34 @@ console.log("checkbox init");
   'use strict';
 
   // Add new item to public Drupal object
+  Drupal.behaviors.faq = {
+    attach: function () {
+
+      // Check if div#lhfs_widget exist
+      if ($('.faq').length > 0) {
+        $(".faq .title").click(function (event) {
+          event.stopPropagation();
+          var radio = $("input[name=show]", this.parentNode)[0];
+          if (radio.checked) {
+            radio.checked = false;
+            return false; //prevents, together with .stopPropagation, bubbling, which could keep the radio button checked, instead of having it set to false here
+          }
+        });
+      }
+    }
+  };
+
+})(jQuery);
+
+/**
+ * LHFS script
+ */
+
+// Closure with jQuery support
+(function($) {
+  'use strict';
+
+  // Add new item to public Drupal object
   Drupal.behaviors.lhfs_widget = {
     attach: function () {
 

6f8761fa8fd3c96e19e0a13bac2050b6cad2fe27
yogibimbi <yogibimbi@qan.be>
2015-06-30 07:06:02 +0200

    update dist and submodules HEADs, implement address switch between NL and foreign addresses, Jira#CXDEVII-688

--- dist/scripts/aegon-library.js
+++ dist/scripts/aegon-library.js
@@ -18578,8 +18578,18 @@ PointerEventsPolyfill.prototype.register_mouse_events = function(){
     attach: function () {
 console.log("attach");
       $("input[name=ra_NL]").click( function () {
-console.log("changed ra " + this);
+        var NL = parseInt($(this).val()) > 0;
+        $(".address .residential .NL").toggleClass("visible", NL);
+        $(".address .residential .world").toggleClass("visible", !NL);
       });
+      $("input[name=ra_NL]").click();
+
+      $("input[name=ca_NL]").click( function () {
+        var NL = parseInt($(this).val()) > 0;
+        $(".address .correspondential .NL").toggleClass("visible", NL);
+        $(".address .correspondential .world").toggleClass("visible", !NL);
+      });
+      $("input[name=ca_NL]").click();
     }
   };
 

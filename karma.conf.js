240bd8ad0cc6cced07b43aae9af585a839467f32
Rik Toetenel <i.m@awesome.yeah>
2016-03-11 16:04:52 +0100

    Updated Karma Conf and Head Links

--- karma.conf.js
+++ karma.conf.js
@@ -6,6 +6,7 @@ module.exports = function(config) {
     files: [
       'lib/aegon-frontend-library/aegon-scripts-library/vendor/drupal_misc/jquery.js',
       'lib/aegon-frontend-library/aegon-scripts-library/vendor/drupal_misc/drupal.js',
+      'lib/aegon-frontend-library/aegon-scripts-library/vendor/wNumb.js',
       'lib/aegon-frontend-library/aegon-scripts-library/vendor/drupal_misc/**/*.js',
       'lib/aegon-frontend-library/aegon-scripts-library/components/quickquote-lijfrente.js',
       'lib/aegon-frontend-library/aegon-scripts-library/components/quickquote-lijfrente-sparen.js',
